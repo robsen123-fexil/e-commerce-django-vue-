843782626af6f305a3a2bb0a94bd8a7c4464dde9
robsen123-fexil <robsenteshoma1@gmail.com>
2024-01-01 08:41:31 +0300

    models

--- base/shoping/models.py
+++ base/shoping/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
 
-# Create your models here.
+class category(models.Model):
+    name=models.CharField(max_length=250)
+    slug=models.SlugField()
+    class Meta:
+        ordering=('name',)
+    def __str__(self):
+        return self.name   
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+class product(models.Model):
+    category=models.ForeignKey(category,related_name='products', on_delete=models.CASCADE)
+    name =models.TextField(max_length=255)
+    slug=models.SlugField()
+    description=models.TextField(blank=True, null=True)
+    price=models.DecimalField(max_digits=2, decimal_places=2)
+    image=models.ImageField(upload_to ='uploads/', blank=True, null=True)
+    thumbnail=models.ImageField(upload_to= 'uploads/', blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    
+
+    class Meta:
+        ordering =('date',)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000'+self.image.url
+        else:
+            return ''
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://127.0.0.1:8000'+self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail= self.make.thumbnail(self.image)
+                self.save()
+                return 'http://127.0.0.1:8000/' + self.thumbnail.url
+            else:
+                return ''
+    def makethumbnail(self, image , size=(300, 200)):
+        img=Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)

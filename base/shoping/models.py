4827d20fa4d2bbc670438c3301264694b945b5dc
robsen123-fexil <robsenteshoma1@gmail.com>
2024-01-01 16:39:42 +0300

    models

--- base/shoping/models.py
+++ base/shoping/models.py
@@ -48,3 +48,6 @@ class product(models.Model):
         img=Image.open(image)
         img.convert('RGB')
         img.thumbnail(size)
+        thumb_io=BytesIO()
+        img.save(thumb_io, 'JPEG' , quality = 85)
+        thumbnail
